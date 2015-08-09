03031242118163ec1f6cccbecd0313798da75454
Madhusanka Goonathilake <madhusanka@localhost.localdomain>
2015-08-10 03:24:31 +0530

    Made some minor improvements to admin dashboard

--- app/cache/dev/twig/4/4/442d8bf28333a5b61646628d6b016effb2ec7c664f9ceb8559c51db6076b5dec.php
+++ app/cache/dev/twig/4/4/442d8bf28333a5b61646628d6b016effb2ec7c664f9ceb8559c51db6076b5dec.php
@@ -23,31 +23,31 @@ class __TwigTemplate_442d8bf28333a5b61646628d6b016effb2ec7c664f9ceb8559c51db6076
 
     protected function doDisplay(array $context, array $blocks = array())
     {
-        $__internal_f7c089b79946b8409005eaf0399e7d55a1c379307b0d3edc251b3d6edef99804 = $this->env->getExtension("native_profiler");
-        $__internal_f7c089b79946b8409005eaf0399e7d55a1c379307b0d3edc251b3d6edef99804->enter($__internal_f7c089b79946b8409005eaf0399e7d55a1c379307b0d3edc251b3d6edef99804_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "FreeVRSShuttleRoutingBundle:Default:todays-shuttle-schedule.html.twig"));
+        $__internal_f9273497e97d515ff1cbcad4f0256c94be246130e6369c633b4b4346a13029bc = $this->env->getExtension("native_profiler");
+        $__internal_f9273497e97d515ff1cbcad4f0256c94be246130e6369c633b4b4346a13029bc->enter($__internal_f9273497e97d515ff1cbcad4f0256c94be246130e6369c633b4b4346a13029bc_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "FreeVRSShuttleRoutingBundle:Default:todays-shuttle-schedule.html.twig"));
 
         $this->parent->display($context, array_merge($this->blocks, $blocks));
         
-        $__internal_f7c089b79946b8409005eaf0399e7d55a1c379307b0d3edc251b3d6edef99804->leave($__internal_f7c089b79946b8409005eaf0399e7d55a1c379307b0d3edc251b3d6edef99804_prof);
+        $__internal_f9273497e97d515ff1cbcad4f0256c94be246130e6369c633b4b4346a13029bc->leave($__internal_f9273497e97d515ff1cbcad4f0256c94be246130e6369c633b4b4346a13029bc_prof);
 
     }
 
     // line 3
     public function block_nav_menu($context, array $blocks = array())
     {
-        $__internal_a04deb304265528c5fe1082814384d0bf87f8f55f40680d6b01c0a4bc81f4a74 = $this->env->getExtension("native_profiler");
-        $__internal_a04deb304265528c5fe1082814384d0bf87f8f55f40680d6b01c0a4bc81f4a74->enter($__internal_a04deb304265528c5fe1082814384d0bf87f8f55f40680d6b01c0a4bc81f4a74_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "nav_menu"));
+        $__internal_9aacb142adf433f0c24296750279da6b8a851059cbe98c508657b9d83e6aca20 = $this->env->getExtension("native_profiler");
+        $__internal_9aacb142adf433f0c24296750279da6b8a851059cbe98c508657b9d83e6aca20->enter($__internal_9aacb142adf433f0c24296750279da6b8a851059cbe98c508657b9d83e6aca20_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "nav_menu"));
 
         
-        $__internal_a04deb304265528c5fe1082814384d0bf87f8f55f40680d6b01c0a4bc81f4a74->leave($__internal_a04deb304265528c5fe1082814384d0bf87f8f55f40680d6b01c0a4bc81f4a74_prof);
+        $__internal_9aacb142adf433f0c24296750279da6b8a851059cbe98c508657b9d83e6aca20->leave($__internal_9aacb142adf433f0c24296750279da6b8a851059cbe98c508657b9d83e6aca20_prof);
 
     }
 
     // line 6
     public function block_stylesheets($context, array $blocks = array())
     {
-        $__internal_e73becb4c95f42d5a390d3aa37689c6fd50d4b162d7e07779feea52852396056 = $this->env->getExtension("native_profiler");
-        $__internal_e73becb4c95f42d5a390d3aa37689c6fd50d4b162d7e07779feea52852396056->enter($__internal_e73becb4c95f42d5a390d3aa37689c6fd50d4b162d7e07779feea52852396056_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "stylesheets"));
+        $__internal_91fe1bb57b5126d9847666810d3c7a604d46f43608429e2424f88a157e0fedb1 = $this->env->getExtension("native_profiler");
+        $__internal_91fe1bb57b5126d9847666810d3c7a604d46f43608429e2424f88a157e0fedb1->enter($__internal_91fe1bb57b5126d9847666810d3c7a604d46f43608429e2424f88a157e0fedb1_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "stylesheets"));
 
         // line 7
         echo "<style type=\"text/css\">
@@ -59,15 +59,15 @@ class __TwigTemplate_442d8bf28333a5b61646628d6b016effb2ec7c664f9ceb8559c51db6076
 </style>
 ";
         
-        $__internal_e73becb4c95f42d5a390d3aa37689c6fd50d4b162d7e07779feea52852396056->leave($__internal_e73becb4c95f42d5a390d3aa37689c6fd50d4b162d7e07779feea52852396056_prof);
+        $__internal_91fe1bb57b5126d9847666810d3c7a604d46f43608429e2424f88a157e0fedb1->leave($__internal_91fe1bb57b5126d9847666810d3c7a604d46f43608429e2424f88a157e0fedb1_prof);
 
     }
 
     // line 16
     public function block_content($context, array $blocks = array())
     {
-        $__internal_68053a324ad07041d43a060c68b24d26e29a393c88f5b56a1032f9509fe8ee60 = $this->env->getExtension("native_profiler");
-        $__internal_68053a324ad07041d43a060c68b24d26e29a393c88f5b56a1032f9509fe8ee60->enter($__internal_68053a324ad07041d43a060c68b24d26e29a393c88f5b56a1032f9509fe8ee60_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "content"));
+        $__internal_0d60693b0a3ea41b84ee05e10fe60b14c240b6219c09b6f9e466bc8c75db868a = $this->env->getExtension("native_profiler");
+        $__internal_0d60693b0a3ea41b84ee05e10fe60b14c240b6219c09b6f9e466bc8c75db868a->enter($__internal_0d60693b0a3ea41b84ee05e10fe60b14c240b6219c09b6f9e466bc8c75db868a_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "content"));
 
         // line 17
         echo "<div class=\"container\">
@@ -87,13 +87,13 @@ class __TwigTemplate_442d8bf28333a5b61646628d6b016effb2ec7c664f9ceb8559c51db6076
             </div>
         </div>
         <div class=\"col l4 m6 s12\">
-            <div class=\"card green lighten-2\">
+            <div class=\"card light-green lighten-1\">
                 <div class=\"card-content center\">
                     <h3 class=\"light\">07:30 PM</h3>
                 </div>
                 <div class=\"card-action center\">
-                    <a href=\"#\" class=\"black-text\">66 people</a>
-                    <a href=\"#\" class=\"black-text\">5 routes</a>
+                    <a href=\"#\" class=\"green-text text-darken-4\">66 people</a>
+                    <a href=\"#\" class=\"green-text text-darken-4\">5 routes</a>
                 </div>
             </div>
         </div>
@@ -165,7 +165,7 @@ class __TwigTemplate_442d8bf28333a5b61646628d6b016effb2ec7c664f9ceb8559c51db6076
 
 ";
         
-        $__internal_68053a324ad07041d43a060c68b24d26e29a393c88f5b56a1032f9509fe8ee60->leave($__internal_68053a324ad07041d43a060c68b24d26e29a393c88f5b56a1032f9509fe8ee60_prof);
+        $__internal_0d60693b0a3ea41b84ee05e10fe60b14c240b6219c09b6f9e466bc8c75db868a->leave($__internal_0d60693b0a3ea41b84ee05e10fe60b14c240b6219c09b6f9e466bc8c75db868a_prof);
 
     }
 
